9d51619be70f6a496220df49783910f87c39c26c
Martin Pons Mayol <marti.pm93@gmail.com>
2022-03-07 22:32:13 +0100

    add sendgrid configuration

--- src/main/java/com/slashmobility/test/web/endpoint/UserEndpoint.java
+++ src/main/java/com/slashmobility/test/web/endpoint/UserEndpoint.java
@@ -1,4 +1,27 @@
 package com.slashmobility.test.web.endpoint;
 
+import com.slashmobility.test.service.UserService;
+import com.slashmobility.test.web.dto.UserDTO;
+import com.slashmobility.test.web.endpoint.constants.URLConstants;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value = URLConstants.USER_ROOT)
 public class UserEndpoint {
+
+    private final UserService userService;
+
+    public UserEndpoint(UserService userService) {
+        this.userService = userService;
+    }
+
+    @PostMapping(produces = MediaType.APPLICATION_JSON_VALUE)
+    public void newUser(@RequestBody UserDTO userDTO) {
+        userService.newUser(userDTO);
+    }
+
 }
---
+++ src/main/java/com/slashmobility/test/web/endpoint/constants/URLConstants.java
@@ -0,0 +1,10 @@
+package com.slashmobility.test.web.endpoint.constants;
+
+public final class URLConstants {
+
+    private URLConstants() {}
+
+    public static final String USER_ROOT = "/users";
+    public  static final String COMPANY_ROOT = "/companies";
+    public static final String PRODUCT_ROOT = "/products";
+}
